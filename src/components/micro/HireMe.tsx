ae696fda70fd2cbbc21eb9f6f6cfc61745ed12c5
Atomox_ <aoxresell@gmail.com>
2022-09-29 14:39:06 +0200

    Hire me button

---
+++ src/components/micro/HireMe.tsx
@@ -0,0 +1,11 @@
+import '../../styles/micro/hireMe.scss';
+
+const HireMe = () => {
+    return (
+        <div className="gradient">
+            Hire me!
+        </div>
+    );  
+};
+
+export { HireMe }; 
--- src/components/micro/TimelineEvent.tsx
+++ src/components/micro/TimelineEvent.tsx
@@ -1,4 +1,32 @@
-const TimelineEvent = () => {
-};
+interface TimelineEventProps {
+    title: string;
+    details?: string;
+    date: string;
+}
+
+const TimelineEvent = (props: TimelineEventProps) => {
+    return (
+        <div className="flex--row event">
+            <div className="flex--row name">
+                <div className="title">
+                    {props.title}
+                </div>
+                {props.details && (
+                    <>
+                        <div>
+                            -
+                        </div>
+                        <div className="details">
+                            {props.details}
+                        </div>
+                    </>
+                )}
+            </div>
+            <div className="date">
+                {props.date}
+            </div>
+        </div>
+    )
+};  
 
 export { TimelineEvent };
